a7e585ab6cbbe929f662edd13d3418a8169af263
alexandre-Maury <154742720+alexandre-Maury@users.noreply.github.com>
2023-12-25 23:38:06 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -35,17 +35,18 @@ done
 
 # Créer le dossier de sauvegarde
 printf "%s \\n" "[En cours] création du dossier de sauvegarde"
-mkdir -p /home/${utilisateur}/.config/zsh/save 
+mkdir -p /home/"${utilisateur}"/.config/zsh/save 
 
 # Sauvegarde des fichier de base 
 printf "%s \\n" "[En cours] Sauvegarde du fichier .zshrc"
-cp -rf /home/${utilisateur}/.zshrc /home/${utilisateur}/.config/zsh/save/.zshrc-backup-$(date +"%Y-%m-%d")
+cp -rf /home/"${utilisateur}"/.zshrc /home/"${utilisateur}"/.config/zsh/save/.zshrc-backup-$(date +"%Y-%m-%d")
 
 
 # Vérifier si le shell actuel est déjà zsh
 if [ "$(basename "$SHELL")" != "zsh" ]; then
     # Changer le shell de l'utilisateur à zsh
     sudo chsh -s $(which zsh) && echo $SHELL
+    printf "%s %s \\n" "[Succès] Shell configuré" "==> $SHELL"
 else
     printf "%s %s \\n" "[Succès] Le shell est déjà configuré" "==> $SHELL"
 
@@ -59,29 +60,28 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # Install themes (powerlevel10k)
 printf "%s %s \\n" "[En cours] Installation du theme" "==> powerlevel10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/home/${utilisateur}/.config/zsh/oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/home/"${utilisateur}"/.config/zsh/oh-my-zsh/custom}/themes/powerlevel10k
 
 
 # Install plugins (zsh-autosuggestions and zsh-syntax-highlighting)
 plugins=("zsh-autosuggestions" "zsh-syntax-highlighting")
 for plugin in "${plugins[@]}"; do
     printf "%s %s \\n" "[En cours] Installation du plugin" "==> ${plugin}"
-    git clone https://github.com/zsh-users/${plugin}.git ${ZSH_CUSTOM:-/home/${utilisateur}/.config/zsh/oh-my-zsh/custom}/plugins/${plugin}
+    git clone https://github.com/zsh-users/"${plugin}".git ${ZSH_CUSTOM:-/home/"${utilisateur}"/.config/zsh/oh-my-zsh/custom}/plugins/"${plugin}"
 done
 
 # Installer fzf
 printf "%s \\n" "[En cours] Installation de fzf"
-git clone https://github.com/junegunn/fzf.git /home/${utilisateur}/.config/fzf
-yes | /home/${utilisateur}/.config/fzf/install
+git clone https://github.com/junegunn/fzf.git /home/"${utilisateur}"/.config/fzf
+yes | /home/"${utilisateur}"/.config/fzf/install
                 
 # Activation du theme 
 printf "%s %s \\n" "[En cours] Activation du theme" "==> powerlevel10k"
-# sed  -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' $HOME/.zshrc
-sed  -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' /home/${utilisateur}/.zshrc
+sed  -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' /home/"${utilisateur}"/.zshrc
 
 # Activation des plugins (zsh-autosuggestions et zsh-syntax-highlighting)
 printf "%s %s \\n" "[En cours] Activation des plugins" "==> zsh-autosuggestions et zsh-syntax-highlighting"
-sed  -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+sed  -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' /home/"${utilisateur}"/.zshrc
                 
 # Copie des fichier dans le dossier ${utilisateur}
 # cp -rf $HOME/.config/zsh/oh-my-zsh /home/"${utilisateur}"/.config/zsh
