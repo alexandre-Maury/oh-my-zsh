c9dec9b9402bf15aa49bf3ce5db224b1a8e55a66
alexandre-Maury <154742720+alexandre-Maury@users.noreply.github.com>
2023-12-25 02:16:46 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -100,8 +100,8 @@ else
         fi
 
         # Cloner oh-my-zsh
-        git clone https://github.com/ohmyzsh/ohmyzsh.git /home/"$utilisateur"/.config/oh-my-zsh/ 
-        sh -c "$(/home/"$utilisateur"/.config/oh-my-zsh/tools/install.sh)"
+        git clone https://github.com/ohmyzsh/ohmyzsh.git /home/"$utilisateur"/.config/oh-my-zsh/
+        /home/"$utilisateur"/.config/oh-my-zsh/tools/install.sh | bash
 
         # Cloner et déplacer le thème Bullet Train
         git clone --depth=1 https://github.com/caiogondim/bullet-train.zsh.git /tmp/bullet-train.zsh
