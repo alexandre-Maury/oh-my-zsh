7f8312386591cab872290827f985c841857cdd9a
alexandre-Maury <154742720+alexandre-Maury@users.noreply.github.com>
2023-12-26 01:16:59 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -83,8 +83,9 @@ sed  -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' 
 printf "%s %s \\n" "[En cours] Activation des plugins" "==> zsh-autosuggestions et zsh-syntax-highlighting"
 sed  -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' /home/"${utilisateur}"/.zshrc
 
-sed  -i 's/export ZSH=$HOME/.oh-my-zsh//home/"${utilisateur}"/.config/zsh/oh-my-zsh/g' /home/"${utilisateur}"/.zshrc
-                
+printf "%s %s \\n" "[En cours] Modification du point d'entrée" "export ZSH=$HOME/.oh-my-zsh ==> export ZSH=/home/"${utilisateur}"/.config/zsh/oh-my-zsh"
+sed -i 's,ZSH=$HOME/.oh-my-zsh,ZSH=/home/alexandre/.config/zsh/oh-my-zsh,' /home/alexandre/.zshrc
+
 # Copie des fichier dans le dossier ${root}
 printf "%s %s \\n" "[En cours] Copie des fichier" "==> root"
 #sudo cp -rf /home/"${utilisateur}"/.config/zsh/oh-my-zsh /root/.config/zsh/oh-my-zsh
